65d997c38bb437f746bec8b14594d0743c290689
AlienRouge <rudov.vlad2013@gmail.com>
2021-05-27 21:02:42 +0700

    Add project files.

---
+++ Hospital-web/Models/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hospital_web.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Diagnosis is required")]
+        public string Diagnosis { get; set; }
+    }
+}
